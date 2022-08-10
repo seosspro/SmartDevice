bdf691592337c3c834e8634cb41ac51e293df4a4
Дмитрий <kdimonr@mail.ru>
2022-08-10 13:35:24 +0700

    fixed footer(borders)

--- build/js/main.js
+++ build/js/main.js
@@ -30,38 +30,6 @@
     accordionToggleButton.addEventListener('click', toggleAccordion);
   });
 })();
-(function () {
-  let accordionItems = document.querySelectorAll('.accordion');
-  let accordionPanes = document.querySelectorAll('.accordion__pane');
-
-  let hidePane = function (button, pane) {
-    button.classList.add('accordion__toggle--inactive');
-    pane.classList.add('accordion__pane--hidden');
-  };
-
-  let showPane = function (button, pane) {
-    button.classList.remove('accordion__toggle--inactive');
-    pane.classList.remove('accordion__pane--hidden');
-  };
-
-  let toggleAccordion = function (evt) {
-    Array.prototype.forEach.call(accordionPanes, function (accordionPane) {
-      let button = accordionPane.closest('.accordion').querySelector('.accordion__toggle');
-      if (button === evt.target && !button.classList.contains('accordion__toggle--inactive') || button !== evt.target) {
-        hidePane(button, accordionPane);
-      } else if (button === evt.target) {
-        showPane(button, accordionPane);
-      }
-    });
-  };
-
-  Array.prototype.forEach.call(accordionItems, function (accordion) {
-    let accordionToggleButton = accordion.querySelector('.accordion__toggle');
-    let accordionPane = accordion.querySelector('.accordion__pane');
-    hidePane(accordionToggleButton, accordionPane);
-    accordionToggleButton.addEventListener('click', toggleAccordion);
-  });
-})();
 
 (function () {
   let KEYCODE = {
