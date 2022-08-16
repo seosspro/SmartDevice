8febd29e02f89e16593abadec086427f9a2864e6
Дмитрий <kdimonr@mail.ru>
2022-08-16 14:24:51 +0700

    исправил ошибки

--- source/js/main.js
+++ source/js/main.js
@@ -14,8 +14,14 @@
 
   let toggleAccordion = function (evt) {
     Array.prototype.forEach.call(accordionPanes, function (accordionPane) {
-      let button = accordionPane.closest('.accordion').querySelector('.accordion__toggle');
-      if (button === evt.target && !button.classList.contains('accordion__toggle--inactive') || button !== evt.target) {
+      let button = accordionPane
+          .closest('.accordion')
+          .querySelector('.accordion__toggle');
+      if (
+        (button === evt.target &&
+          !button.classList.contains('accordion__toggle--inactive')) ||
+        button !== evt.target
+      ) {
         hidePane(button, accordionPane);
       } else if (button === evt.target) {
         showPane(button, accordionPane);
@@ -37,72 +43,74 @@
   };
   let link = document.querySelector('.page-header__contacts-button');
   let popup = document.querySelector('.modal');
-  let close = popup.querySelector('.modal__close');
-  let form = popup.querySelector('.modal__form');
-  let userName = popup.querySelector('#call-name');
-  let phone = popup.querySelector('#call-phone');
-  let message = popup.querySelector('#call-question');
-  let isStorageSupport = true;
-  let storage = {};
+  if (popup) {
+    let close = popup.querySelector('.modal__close');
+    let form = popup.querySelector('.modal__form');
+    let userName = popup.querySelector('#call-name');
+    let phone = popup.querySelector('#call-phone');
+    let message = popup.querySelector('#call-question');
+    let isStorageSupport = true;
+    let storage = {};
 
-  let openPopup = function () {
-    popup.classList.add('modal--show');
-    document.body.classList.add('disable-scroll');
-  };
+    let openPopup = function () {
+      popup.classList.add('modal--show');
+      document.body.classList.add('disable-scroll');
+    };
 
-  let closePopup = function () {
-    popup.classList.remove('modal--show');
-    document.body.classList.remove('disable-scroll');
-  };
-  try {
-    storage.name = localStorage.getItem('name');
-    storage.phone = localStorage.getItem('phone');
-    storage.message = localStorage.getItem('message');
-  } catch (err) {
-    isStorageSupport = false;
-  }
-
-  link.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    openPopup();
-
-    if (storage.name) {
-      userName.value = storage.name;
-      phone.value = storage.phone;
-      message.value = storage.message;
-      message.focus();
-    } else {
-      userName.focus();
+    let closePopup = function () {
+      popup.classList.remove('modal--show');
+      document.body.classList.remove('disable-scroll');
+    };
+    try {
+      storage.name = localStorage.getItem('name');
+      storage.phone = localStorage.getItem('phone');
+      storage.message = localStorage.getItem('message');
+    } catch (err) {
+      isStorageSupport = false;
     }
-  });
 
-  close.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    closePopup();
-  });
-
-  form.addEventListener('submit', function () {
-    if (isStorageSupport) {
-      localStorage.setItem('name', userName.value);
-      localStorage.setItem('phone', phone.value);
-      localStorage.setItem('message', message.value);
-    }
-  });
-
-  window.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === KEYCODE.esc) {
+    link.addEventListener('click', function (evt) {
       evt.preventDefault();
-      if (popup.classList.contains('modal--show')) {
+      openPopup();
+
+      if (storage.name) {
+        userName.value = storage.name;
+        phone.value = storage.phone;
+        message.value = storage.message;
+        message.focus();
+      } else {
+        userName.focus();
+      }
+    });
+
+    close.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      closePopup();
+    });
+
+    form.addEventListener('submit', function () {
+      if (isStorageSupport) {
+        localStorage.setItem('name', userName.value);
+        localStorage.setItem('phone', phone.value);
+        localStorage.setItem('message', message.value);
+      }
+    });
+
+    window.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === KEYCODE.esc) {
+        evt.preventDefault();
+        if (popup.classList.contains('modal--show')) {
+          closePopup();
+        }
+      }
+    });
+
+    popup.addEventListener('click', function (evt) {
+      if (evt.target === popup) {
         closePopup();
       }
-    }
-  });
-
-  popup.addEventListener('click', function (evt) {
-    if (evt.target === popup) {
-      closePopup();
-    }
-  });
+    });
+  }
 })();
 
 let btnShow = document.querySelector('.about-us__button');
